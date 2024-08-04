dc15071cb38adc4cdee0407dec8aae90b6e6fc7a
yolo5128 <yolofrmcord@gmail.com>
2024-08-03 19:01:02 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,9 +7,7 @@ local function LoadGame(GameId)
     local GameName = Games[tostring(GameId)]
     if GameName then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/yolo5128/HumilityCC/main/Games/"..GameName..".lua"))()
-    else
-        warn("Game not supported")
+
     end
-end
 
 LoadGame(game.GameId)
